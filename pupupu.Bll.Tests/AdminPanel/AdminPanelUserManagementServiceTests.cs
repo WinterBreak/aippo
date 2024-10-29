b85db5a7a7bee879c4a0aaf7ad20b7f7a25cd9e9
Elizaveta Lozhkina <liza.lojkina@yandex.ru>
2024-10-29 20:54:31 +0500

    Добавление либ для тестов, проекта для тестов, папок для тестов — всё для тестов

---
+++ pupupu.Bll.Tests/AdminPanel/AdminPanelUserManagementServiceTests.cs
@@ -0,0 +1,16 @@
+namespace pupupu.Bll.Tests;
+
+[TestFixture]
+public class AdminPanelUserManagementServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
